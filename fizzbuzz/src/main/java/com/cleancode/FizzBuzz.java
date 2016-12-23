b0aca98a97d5ab9713a993a7f2eca858506c1685
Sankaran M K <sankaran.mullamamgalath@in.tesco.com>
2016-12-23 12:39:25 +0530

    Refactoring code

--- fizzbuzz/src/main/java/com/cleancode/FizzBuzz.java
+++ fizzbuzz/src/main/java/com/cleancode/FizzBuzz.java
@@ -3,13 +3,21 @@ package com.cleancode;
 public class FizzBuzz {
 	
 	public static String getAnswerFor(int number){
-		if(number%3 == 0){
+		if(isDivisibleByThree(number)){
 			return "Fizz";
 		}
-		if(number%5 == 0){
+		if(isDivisibleByFive(number)){
 			return "Buzz";
 		}
 		return String.valueOf(number);
+	}
+
+	private static boolean isDivisibleByFive(int number) {
+		return number%5 == 0;
+	}
+
+	private static boolean isDivisibleByThree(int number) {
+		return number%3 == 0;
 	}
 
 }
