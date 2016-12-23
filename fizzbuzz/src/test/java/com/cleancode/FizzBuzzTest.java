4c25dc562afb4811056339c3be275393f3484c50
Sankaran M K <sankaran.mullamamgalath@in.tesco.com>
2016-12-23 12:54:58 +0530

    added mor test conditions

--- fizzbuzz/src/test/java/com/cleancode/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/com/cleancode/FizzBuzzTest.java
@@ -11,29 +11,39 @@ public class FizzBuzzTest {
 	public void shouldReturnNumberForNumbersNotDivisbleByThreeAndFive() {
 		assertThat(FizzBuzz.getAnswerFor(1), is("1"));
 		assertThat(FizzBuzz.getAnswerFor(2), is("2"));
+		assertThat(FizzBuzz.getAnswerFor(4), is("4"));
+		assertThat(FizzBuzz.getAnswerFor(7), is("7"));
 	}
 
 	@Test
 	public void shouldReturnFizzForNumbersDivisibleByThree() {
 		assertThat(FizzBuzz.getAnswerFor(3), is("Fizz"));
 		assertThat(FizzBuzz.getAnswerFor(6), is("Fizz"));
+		assertThat(FizzBuzz.getAnswerFor(9), is("Fizz"));
 	}
 
 	@Test
 	public void shouldReturnBizzForNumbersDivisibleByFive() {
 		assertThat(FizzBuzz.getAnswerFor(5), is("Buzz"));
 		assertThat(FizzBuzz.getAnswerFor(10), is("Buzz"));
+		assertThat(FizzBuzz.getAnswerFor(20), is("Buzz"));
 	}
 
 	@Test
 	public void shouldReturnFizzBizzForNumbersDivisbleByFifteen() {
 		assertThat(FizzBuzz.getAnswerFor(15), is("FizzBuzz"));
 		assertThat(FizzBuzz.getAnswerFor(30), is("FizzBuzz"));
+		assertThat(FizzBuzz.getAnswerFor(45), is("FizzBuzz"));
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void shouldThrowErrorForNumbersLessThanOne() {
+	public void shouldThrowErrorForZero() {
 		FizzBuzz.getAnswerFor(0);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void shouldThrowErrorForNegativeNumber() {
+		FizzBuzz.getAnswerFor(-1);
 	}
 
 }
