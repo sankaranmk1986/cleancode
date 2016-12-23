621751cb0444839a70a5068cfbd479b4b028d119
Sankaran M K <sankaran.mullamamgalath@in.tesco.com>
2016-12-23 12:35:19 +0530

    success for number 5

--- fizzbuzz/src/test/java/com/cleancode/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/com/cleancode/FizzBuzzTest.java
@@ -6,29 +6,29 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class FizzBuzzTest {
-	
+
 	@Test
-	public void shouldReturnNumberForOne(){
+	public void shouldReturnNumberForOne() {
 		assertThat(FizzBuzz.getAnswerFor(1), is("1"));
-	}	
-	
+	}
+
 	@Test
-	public void shouldReturnNumberForTwo(){
+	public void shouldReturnNumberForTwo() {
 		assertThat(FizzBuzz.getAnswerFor(2), is("2"));
-	}	
-	
+	}
+
 	@Test
-	public void shouldReturnFizzForThree(){
+	public void shouldReturnFizzForThree() {
 		assertThat(FizzBuzz.getAnswerFor(3), is("Fizz"));
-	}	
-	
+	}
+
 	@Test
-	public void shouldReturnFizzForSix(){
+	public void shouldReturnFizzForSix() {
 		assertThat(FizzBuzz.getAnswerFor(6), is("Fizz"));
 	}
-	
+
 	@Test
-	public void shouldReturnBizzForFive(){
+	public void shouldReturnBizzForFive() {
 		assertThat(FizzBuzz.getAnswerFor(5), is("Buzz"));
-	}	
+	}
 }
