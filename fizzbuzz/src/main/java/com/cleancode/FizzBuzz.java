686ded404527b5a1e0f68c277436590fe183edcb
Sankaran M K <sankaran.mullamamgalath@in.tesco.com>
2016-12-23 12:29:50 +0530

    Success for number 3

--- fizzbuzz/src/main/java/com/cleancode/FizzBuzz.java
+++ fizzbuzz/src/main/java/com/cleancode/FizzBuzz.java
@@ -3,6 +3,9 @@ package com.cleancode;
 public class FizzBuzz {
 	
 	public static String getAnswerFor(int number){
+		if(number == 3){
+			return "Fizz";
+		}
 		return String.valueOf(number);
 	}
 
